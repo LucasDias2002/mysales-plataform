e22ff0403e4de3f863918a21f069a31aa5e0f68c
Lucas Phillipe Freitas Dias <lucas.pfd.2002@gmail.com>
2025-03-12 16:08:25 -0300

    Alterando as respostas do controler (Sale) para retornarem um objeto do tipo ResponseEntity e status corretos.

--- src/main/java/br/com/mysales/mysales_plataform/controller/SaleController.java
+++ src/main/java/br/com/mysales/mysales_plataform/controller/SaleController.java
@@ -4,7 +4,9 @@ import br.com.mysales.mysales_plataform.dto.SaleDTO;
 import br.com.mysales.mysales_plataform.model.Sale;
 import br.com.mysales.mysales_plataform.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -16,22 +18,25 @@ public class SaleController {
     SaleService service;
 
     @GetMapping
-    public List<SaleDTO> list(){
-       return service.list();
+    public ResponseEntity<List<SaleDTO>> list(){
+       return ResponseEntity.ok(service.list());
     }
 
     @PostMapping
-    public SaleDTO create(@RequestBody Sale sale){
-        return service.create(sale);
+    public ResponseEntity<SaleDTO> create(@RequestBody Sale sale, UriComponentsBuilder uriBuilder){
+        var saleDTO = service.create(sale);
+        var uri = uriBuilder.path("/sale/{id}").buildAndExpand(saleDTO.client_id()).toUri();
+        return ResponseEntity.created(uri).body(saleDTO);
     }
 
     @PutMapping
-    public SaleDTO update(@RequestBody Sale sale){
-        return service.update(sale);
+    public ResponseEntity<SaleDTO> update(@RequestBody Sale sale){
+        return ResponseEntity.ok(service.update(sale));
     }
 
     @DeleteMapping("{id}")
-    public SaleDTO delete(@PathVariable Long id){
-        return service.delete(id);
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
